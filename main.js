d20ce244ef299622c719c958dacc9814260e5b45
Andriy <andriy.sachok@gmail.com>
2022-05-18 18:41:40 +0300

    global commit

--- main.js
+++ main.js
@@ -1,62 +1,8 @@
-// Повертає випадкове ціле число в діапазоні [m, n] (включно)
-function rand(m, n) {
-    return m + Math.floor((n - m + 1) * Math.random());
+const printDOM = (node, prefix) => {
+  console.log(prefix + node.nodeName);
+  for (let i = 0; i < node.childNodes.length; i++) {
+    printDOM(node.childNodes[i], prefix + `\t`);
+  }
 }
 
-// Випадково повертає строку, представляючу одну з шести
-// граней Корони та Якоря
-function randFace() {
-    return ["crown", "anchor", "heart", "spade", "club", "diamond"]
-        [rand(0, 5)];
-}
-
-let funds = 50; // Початкова умова
-let round = 0;
-
-while (funds > 1 && funds < 100) {
-    round++;
-    console.log(`round ${round}:`);
-    console.log(`\tstarting funds: ${funds}p`);
-
-    // Розташування ставок
-    let bets = {crown: 0, anchor: 0, heart: 0,
-        spade: 0, club: 0, diamond: 0};
-    let totalBet = rand(1, funds);
-    if(totalBet === 7) {
-        totalBet = funds;
-        bets.heart = totalBet;
-    } else {
-        // Ділення всих ставок
-        let remaining = totalBet;
-        do {
-            let bet = rand(1, remaining);
-            let face = randFace();
-            bets[face] = bets[face] + bet;
-            remaining = remaining - bet;
-        } while (remaining > 0)
-    }
-    funds = funds - totalBet;
-    console.log(`\tbets: ` +
-    Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(', ') +
-    ` (total: ${totalBet} pence)`);
-
-    // Кидок костей
-    const hand = [];
-    for (let roll = 0; roll < 3; roll++) {
-        hand.push(randFace());
-    }
-
-    console.log(`\thand: ${hand.join(', ')}`);
-
-    // Отримання виграшу
-    let winnings = 0;
-    for (let die = 0; die < hand.length; die++) {
-        let face = hand[die];
-        if(bets[face] > 0) winnings = winnings + bets[face];
-    }
-    
-    funds = funds + winnings;
-    console.log(`\twinnings: ${winnings}`);
-}
-
-console.log(`\tending funs: ${funds}`);
+printDOM(document, '');
