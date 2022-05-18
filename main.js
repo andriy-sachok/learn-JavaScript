8753a67fb741bf009df976003f5e8356e46d36b8
Andriy <andriy.sachok@gmail.com>
2022-05-18 19:21:43 +0300

    added class higllight

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const printDOM = (node, prefix) => {
-  console.log(prefix + node.nodeName);
-  for (let i = 0; i < node.childNodes.length; i++) {
-    printDOM(node.childNodes[i], prefix + `\t`);
+const highlightParas = (containing) => {
+  if (typeof containing === 'string'){
+    containing = new RegExp(`\\b${containing}\\b`, 'i');
+  }
+  const paras = document.querySelectorAll(`p`);
+  console.log(paras);
+  for (let p of paras) {
+    if(!containing.test(p.textContent)) continue;
+    p.classList.add('highlight');
   }
 }
+highlightParas('unique');
 
-printDOM(document, '');
+const removeParaHighlights = () => {
+ const paras = document.querySelectorAll(`p.highlight`);
+ for (let p of paras){
+   p.classList.remove(`highlight`);
+ } 
+}
