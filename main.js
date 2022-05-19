72237705f4945059ec8566c826dfa475a5b9648f
Andriy <andriy.sachok@gmail.com>
2022-05-19 16:53:41 +0300

    added Mark/Unmark string buttons

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 const highlightParas = (containing) => {
-  if (typeof containing === 'string'){
+  if (typeof containing === 'string') {
     containing = new RegExp(`\\b${containing}\\b`, 'i');
   }
   const paras = document.querySelectorAll(`p`);
-  console.log(paras);
+  // console.log(paras);
   for (let p of paras) {
-    if(!containing.test(p.textContent)) continue;
+    if (!containing.test(p.textContent)) continue;
     p.classList.add('highlight');
   }
 }
-highlightParas('unique');
 
 const removeParaHighlights = () => {
- const paras = document.querySelectorAll(`p.highlight`);
- for (let p of paras){
-   p.classList.remove(`highlight`);
- } 
+  const paras = document.querySelectorAll(`p.highlight`);
+  for (let p of paras) {
+    p.classList.remove(`highlight`);
+  }
+}
+
+highlightParas('unique');
+
+const highlightActions = document.querySelectorAll(`[data-action="highlight"]`);
+
+for (let a of highlightActions) {
+  a.addEventListener(`click`, evt => {
+    evt.preventDefault();
+    highlightParas(a.dataset.containing);
+  });
+}
+
+const removeHighlightActions =
+  document.querySelectorAll(`[data-action="removeHighlights"]`);
+
+for (let a of removeHighlightActions) {
+  a.addEventListener('click', evt => {
+    evt.preventDefault();
+    removeParaHighlights();
+  });
 }
